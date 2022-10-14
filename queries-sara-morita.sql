d9ff177c88568599b6895fff4d3c0f3a7fc4f67c
Sara Morita <65134819+sahmo1@users.noreply.github.com>
2022-10-14 04:31:53 -0400

    done

--- queries-sara-morita.sql
+++ queries-sara-morita.sql
@@ -5,8 +5,6 @@ rental.rental_id  = payment.rental_id and
 rental.inventory_id = inventory.inventory_id 
 group by inventory.film_id;
 
-
-
 -- Create a view named total_rental_amount using the query from the previous step.
 CREATE VIEW total_rental_amount AS
 SELECT inventory.film_id, sum(payment.amount) as total_amount from
@@ -14,7 +12,6 @@ rental, payment, inventory where
 rental.rental_id  = payment.rental_id and 
 rental.inventory_id = inventory.inventory_id 
 group by inventory.film_id;
-
 
 -- the total number of copies in inventory for each film. 
 -- Your query should return the following columns: film_id total_copies
@@ -27,7 +24,6 @@ CREATE VIEW total_film_copies AS
 SELECT film_id, count(inventory_id) as total_copies from
  inventory
 group by film_id;
-
 
 -- find all films with a total rental amount greater than 200.00,
 SELECT film.film_id, title, description, rental_duration, rental_rate, 
